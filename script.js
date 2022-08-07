a2eb5b6e08771523e6e7c619552e2c1bc07a3b0a
CarlosPrieto <carlosprieto66@icloud.com>
2022-08-07 16:57:16 -0700

    v1 finished

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 "use strict"
+// Grabbing dom elements
+const pxInput = document.querySelector('#px-input');
+const remInput = document.querySelector('#rem-input');
 
+
+// Converting functions
 const pxOutput = remInput => remInput * 16;
-
 const remOutput = pxInput => pxInput / 16;
 
+// EventListeners will trigger function that changes the value of the inputs.
+pxInput.addEventListener('input', (e) => {
+    const pxValue = e.target.value;
+    return remInput.value = remOutput(pxValue);
+});
+
+remInput.addEventListener('input', (e) => {
+    const remValue = e.target.value;
+    return pxInput.value = pxOutput(remValue);
+});
